2ab50d338f61caf6bc1d919e1668ad05e4e3f6e1
LiuHai0092 <294488725@qq.com>
2025-05-18 15:14:51 +0800

    scoped样式

---
+++ day05/vue-demo2/01-scoped样式独立/components/BaseOne.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="base-one">BaseOne</div>
+</template>
+
+<script>
+export default {
+  name: "BaseOne",
+};
+</script>
+
+<style scoped>
+div {
+  border: 3px solid blue;
+  margin: 30px;
+}
+</style>
---
+++ day05/vue-demo2/01-scoped样式独立/components/BaseTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="base-one">
+    BaseTwo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseGoodsItem',
+}
+</script>
+
+<style>
+div{
+  border: 3px solid red;
+  margin: 30px;
+ }
+</style>
